a6ab1b50ad3233cc754b0902755e157c3345135c
Humayra Tapali <humayra.tapali@kcl.ac.uk>
2023-10-24 13:42:20 +0100

    Implement frequencies function

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*args):
     frequencies = {}
-    # Your code goes here
+    
+    item_list = [*args]
+    string_list = [str(item) for item in item_list]
+    #print(string_list)
+    
+    for str_item in string_list:
+        if(str_item in frequencies):
+            frequencies[str_item] += 1
+        else:
+            frequencies[str_item] = 1
+    
     return frequencies
+
+print(frequencies(1, 7, 7, 7, 8, 8, 'Hello, World!', False))
