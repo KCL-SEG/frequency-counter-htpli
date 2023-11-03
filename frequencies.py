7fb88f3960c194b97f003267ded268ca83f5e340
Humayra Tapali <humayra.tapali@kcl.ac.uk>
2023-11-03 11:28:20 +0000

    Create frequency counter function

--- frequencies.py
+++ frequencies.py
@@ -2,17 +2,16 @@
 
 def frequencies(*args):
     frequencies = {}
-    
-    item_list = [*args]
-    string_list = [str(item) for item in item_list]
-    #print(string_list)
-    
-    for str_item in string_list:
-        if(str_item in frequencies):
-            frequencies[str_item] += 1
+    for item in args:
+        if item in frequencies:
+            frequencies[item] += 1
         else:
-            frequencies[str_item] = 1
-    
+            frequencies[item] = 1
     return frequencies
 
-print(frequencies(1, 7, 7, 7, 8, 8, 'Hello, World!', False))
+# print(frequencies(1, 7, 7, 7, 8, 8, 'Hello, World!', False))
+# print(frequencies('0', 4,4,'4','d','d','e',0,'a','d','4'))
+# print(frequencies('a', 'a', 'b', 'b', 'b', 'c'))
+# print(frequencies(100, 'Hello', '100', '100', 100))
+# print(frequencies(''))
+# print(frequencies())
