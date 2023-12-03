65c60f2310d599a6d795c7c66f60dbaed453c8cc
Humayra Tapali <humayra.tapali@kcl.ac.uk>
2023-12-03 20:55:13 +0000

    Frequencies fucntion

--- frequencies.py
+++ frequencies.py
@@ -1,17 +1,11 @@
 """Frequencies function."""
 
-def frequencies(*args):
+def frequencies(items):
     frequencies = {}
-    for item in args:
-        if item in frequencies:
-            frequencies[item] += 1
+    for item in items:
+        if isinstance(item, str):   #if item is string
+            frequencies[item] = frequencies.get(item, 0) + 1
         else:
-            frequencies[item] = 1
+            item_str = str(item)
+            frequencies[item_str] = frequencies.get(item_str, 0) + 1
     return frequencies
-
-# print(frequencies(1, 7, 7, 7, 8, 8, 'Hello, World!', False))
-# print(frequencies('0', 4,4,'4','d','d','e',0,'a','d','4'))
-# print(frequencies('a', 'a', 'b', 'b', 'b', 'c'))
-# print(frequencies(100, 'Hello', '100', '100', 100))
-# print(frequencies(''))
-# print(frequencies())
